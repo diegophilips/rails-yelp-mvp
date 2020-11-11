c2fe097c2643e74b92e34da93d054508234e82f4
Diego Philips <diego.r.philips@gmail.com>
2020-11-11 15:04:27 +0000

    I like jazz

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-    resources :restaurants
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %i[index new show create] do
+    resources :reviews, only: %i[new create]
+  end
 end
